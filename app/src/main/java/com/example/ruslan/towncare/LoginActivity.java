162ac13d54b09d4754dfaeb874a8ea4fb711382f
Ruslan Gogerman <ruslangox@gmail.com>
2017-05-27 13:06:43 +0300

    changed from mainActivity to LoginActivity

---
+++ app/src/main/java/com/example/ruslan/towncare/LoginActivity.java
@@ -0,0 +1,27 @@
+package com.example.ruslan.towncare;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button btn_signIn = (Button) findViewById(R.id.btn_signIn);
+
+        btn_signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent intent = new Intent(LoginActivity.this, StudentDetailsActivity.class);
+
+            }
+        });
+
+    }
+}
